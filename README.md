4e875f706be49c3401eb277cbf97e234fd3d1324
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-14 17:43:12 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Repositorio con ejemplos para clase de Introducción a la programación
+con Python, del curso de Tópicos de Física I, Universidad de Costa Rica.
+
+Prof: Jeudy Blanco
---
+++ ejemplos0415.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""
+Script simple para demostrar conceptos básicos
+del lenguaje Python. Temas incluidos:
+    -Variables (enteros, cadenas, booleanos)
+    -Operadores matemáticos y lógicos
+    -Estructuras de datos (listas, diccionarios, conjuntos, tuplas)
+"""
+
+
