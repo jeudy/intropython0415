f73a627e0bd406074720a8539b08c37065c07087
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-14 17:50:20 -0600

    Agregando codigo

--- ejemplos0415.py
+++ ejemplos0415.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 """
 Script simple para demostrar conceptos básicos
@@ -7,5 +8,43 @@ del lenguaje Python. Temas incluidos:
     -Operadores matemáticos y lógicos
     -Estructuras de datos (listas, diccionarios, conjuntos, tuplas)
 """
+
+valor1 = 1000 #entero
+valor2 = 2000
+valor1 += valor2
+potencia = 10**3
+
+print valor1
+print valor2
+print potencia
+
+cadena = "Los valores que tengo son %s, %s y la potencia: %s"%(valor1, valor2, potencia)
+
+print cadena
+
+cadena_upper = cadena.upper()
+
+print cadena_upper
+
+resultado = valor1 > valor2
+booleano = not resultado
+
+print resultado, booleano
+
+str_lista_paises = "CR,MX,AR,US,IT"
+
+lista_paises = str_lista_paises.split(",")
+
+lista_paises.sort()
+
+for pais in lista_paises:
+    print pais
+
+lista_paises.reverse()
+
+for pais in lista_paises:
+    print pais
+
+print "El tamaño de la lista es: %d"%(len(lista_paises))
 
 
