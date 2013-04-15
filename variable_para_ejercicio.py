6eaa7f63878a8ff1e6e982e49055e70374385b3e
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-14 20:34:45 -0600

    Correccion

--- variable_para_ejercicio.py
+++ variable_para_ejercicio.py
@@ -1,5 +1,5 @@
 texto = """Yo soy una maquina, todas mis funciones fueron programadas, 
-tengo setenta años pero luzco como alguien de veinte, ahora no soy solo eso,
+tengo setenta años pero luzco como alguien de veinte, ahora no soy solo eso, 
 soy una maquina humana, con la capacidad de sentir cosas que nunca habia sentido antes. 
 El ser humano en esencia es una maquina, ahora lo se, pues el cuerpo que tengo 
 requiere de muchos cuidados, pues cumple muchas funciones, infinitamente mas, 
