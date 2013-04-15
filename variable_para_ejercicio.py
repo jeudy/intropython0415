2e0b813f3c549d8914f249afd2d8c67b80a2e45b
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-14 20:34:05 -0600

    Correccion

--- variable_para_ejercicio.py
+++ variable_para_ejercicio.py
@@ -4,7 +4,6 @@ soy una maquina humana, con la capacidad de sentir cosas que nunca habia sentido
 El ser humano en esencia es una maquina, ahora lo se, pues el cuerpo que tengo 
 requiere de muchos cuidados, pues cumple muchas funciones, infinitamente mas, 
 de las que cumplia mi anterior cuerpo, ahora soy mortal y entiendo que mi vida 
-tiene un proposito, mi destino es ser el esclavo Reim que se convirtio en humano y
-aprendio que la vida es un espiral, en el cual uno se mueve progresando continuamente,
+tiene un proposito, mi destino es ser el esclavo Reim que se convirtio en humano y 
+aprendio que la vida es un espiral, en el cual uno se mueve progresando continuamente, 
 ahora me dirijo a Prelude, el lugar de origen de los Maquina y el mio tambien, voy en busca del significado de mi existencia."""
-
