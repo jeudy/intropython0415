962d0bc299cba80d8fd2c62cf63befa16a569709
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-19 23:51:37 -0600

    Agrego nuevo ejemplo

---
+++ ejemplo_controldeflujo.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Demostración de uso de control de flujo con expresiones complejas
+Usamos la función randint del módulo random para generar números aleatorios
+"""
+
+import random
+
+numero1 = random.randint(1,1000)
+numero2 = random.randint(1,1000)
+numero3 = random.randint(1,1000)
+
+print numero1, numero2, numero3
+
+if numero1 > numero2 and numero2 > numero3:
+    if numero1 > numero3:
+        print "Se cumple condición 1 de ejemplo"
+else:
+    numero4 = random.randint(1,1000)
+    print numero4
+    if numero4 > (numero1 + numero2) or numero1 ** 3 > 50000:
+        print "Ejemplo de condiciones complejas"
+    
