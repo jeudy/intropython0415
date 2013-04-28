76f874cc7a4cc078567f0c26ca4d5d4ec2a80ac9
Jeudy Blanco Vega <jeudy.blanco@cinespa.ucr.ac.cr>
2013-04-28 00:17:18 -0600

    Mas ejemplos

---
+++ ejemplo_escritura_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+Programa de ejemplo que escribe una linea de números a un archivo en modo w
+Imprimer 10 lineas con los numeros del 0 al 100, 10 números por linea
+"""
+
+numeros = range(0,101)      # Genero una lista con los numeros del 0 al 100
+
+linea = ""
+
+ptr_archivo = open('datos/numeros.txt', 'a')    # Abrimos el archivo en modo append
+
+for n in numeros:
+    linea += ",%s" % (n)
+    if n % 10 == 0 and n != 0:
+        ptr_archivo.write(linea[1:])            # Cada 10 numeros, escribo la linea. El [1:] es para eliminar la primera coma
+        ptr_archivo.write('\n')                 # Tenemos que escribir el cambio de linea!
+        linea = ""
+
+ptr_archivo.close()
--- ejemplo_escritura_so.py
+++ ejemplo_escritura_so.py
@@ -6,13 +6,12 @@ Programa de ejemplo que escribe una linea de números a la salida estandar
 Imprimer 10 lineas con los numeros del 0 al 100, 10 números por linea
 """
 
-numeros = range(0,101)      #Genero una lista con los numeros del 0 al 100
+numeros = range(0,101)      # Genero una lista con los numeros del 0 al 100
 
 linea = ""
 
 for n in numeros:
-    if n % 10 == 0:
-        print linea[1:]     #Cada 10 numeros, imprimo la linea. El [1:] es para eliminar la primera coma
+    linea += ",%s" % (n)
+    if n % 10 == 0 and n != 0:
+        print linea[1:]     # Cada 10 numeros, imprimo la linea. El [1:] es para eliminar la primera coma
         linea = ""
-    else:
-        linea += ",%s" % (n)
